10f10bd4e7fa0acf31216921ed4ff4b1828756d1
室井博也 <>
2021-03-17 14:44:17 +0900

    検索結果表示修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,12 @@ class ItemsController < ApplicationController
   def search
     if params[:keyword].present?
       items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
-      @items_full = []
+      items_full = []
       items.each do |item|
-        if item.name.include?("中古")
-          @items_full.push(item)
-        end
+        items_full.push(item)
       end
-      if @items_full.present?
-       @items = Kaminari.paginate_array(@items_full).page(params[:page]).per(10)
+      if items_full.present?
+       @items = Kaminari.paginate_array(items_full).page(params[:page]).per(10)
       end
     else
       render :search
